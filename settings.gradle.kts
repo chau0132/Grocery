123a15afa24d0813974dedf614a4462a5a998a0d
chau0132 <chau0132@umn.edu>
2025-01-12 13:03:31 -0600

    Add project to github

---
+++ settings.gradle.kts
@@ -0,0 +1,16 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+rootProject.name = "Grocerygit update-index --chmod=+x gradlewgit update-index --chmod=+x gradlew"
+include(":app")
